a45359da648d326659c76d271901756be1c53062
Doron Shapiro <shapiro.doron@gmail.com>
2015-09-05 22:58:16 -0400

    mouse controller

---
+++ useMouse.py
@@ -0,0 +1,9 @@
+import pyautogui
+import math
+
+pyautogui.moveTo(1, 1)
+pyautogui.moveTo(500, 500, 0.5)
+pyautogui.moveTo(1000, 0, 0.5)
+
+# for t in range(0, 100):
+#    pyautogui.moveTo(300 + 100 * math.cos(t), 300 + 100 * math.sin(t), 0.05)
