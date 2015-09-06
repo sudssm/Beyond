8c45444ebf78c40ecdb290bd24db4081c2d418e5
feefles <eyeung@seas.upenn.edu>
2015-09-05 20:16:04 -0400

    add changes to gestures and tracking and update gitignore

--- pennapps/gestures/gesture.py
+++ pennapps/gestures/gesture.py
@@ -5,12 +5,17 @@ gestureMap = {
   ('L',): "left",
   ('R',): "right",
   ('D',): "down",
+  ('UR',): "upright",
+  ('UL',): "upleft",
+  ('DR',): "downright",
+  ('DL',): "downleft",
   ('R', 'D', 'L', 'U',): "sq_cw_tl",
   ('D', 'R', 'U', 'L',): "sq_ccw_tl",
   ('DR', 'L', 'UR',): "tr_cw_top",
   ('R', 'UL', 'DL',): "tr_cw_bl",
-  ('R', 'DR', 'DL', 'UL', 'UR',): "superman",
-  ('DR', 'U', 'DL', 'UR', 'L',): "star"
+  ('UL', 'DL','R',): "tr_cw_br",
+  #('R', 'DR', 'DL', 'UL', 'UR',): "superman",
+  #('DR', 'U', 'DL', 'UR', 'L',): "star"
 }
 
 
@@ -28,31 +33,34 @@ def importPoints (filename):
 
 def lookup (points):
   strokes = moosegesture.getGesture(points)
+  if len(strokes) == 0:
+    return None
+  possibles = gestureMap.keys()
   gestures = moosegesture.findClosestMatchingGesture(strokes, 
     gestureMap.keys(),
-    tolerance=10)
+    tolerance=15)
 
-  #print strokes
+  print strokes
 
   for gesture in gestures:
     return gestureMap[gesture]
 
-print lookup (importPoints("in/star.in"))
+# print lookup (importPoints("in/star.in"))
 
-print lookup (importPoints("in/square.in"))
-print ""
-print lookup (importPoints("in/square2.in"))
-print ""
-print lookup (importPoints("in/square3.in"))
-print ""
-print lookup (importPoints("in/square4.in"))
-print ""
-print lookup (importPoints("in/triangle.in"))
-print ""
+# print lookup (importPoints("in/square.in"))
+# print ""
+# print lookup (importPoints("in/square2.in"))
+# print ""
+# print lookup (importPoints("in/square3.in"))
+# print ""
+# print lookup (importPoints("in/square4.in"))
+# print ""
+# print lookup (importPoints("in/triangle.in"))
+# print ""
 
-print lookup(importPoints("in/superman.in"))
-print lookup(importPoints("in/superman2.in"))
+# print lookup(importPoints("in/superman.in"))
+# print lookup(importPoints("in/superman2.in"))
 
-print lookup(importPoints("in/right.in"))
-print lookup(importPoints("in/down.in"))
+# print lookup(importPoints("in/right.in"))
+# print lookup(importPoints("in/down.in"))
 
--- pennapps/tracking.py
+++ pennapps/tracking.py
@@ -1,11 +1,18 @@
 import cv2
 import numpy as np
+from gestures import gesture
+import cv2.cv as cv
 
 cap = cv2.VideoCapture(0)
 
-gesture = []
+gesture_cache = []
+lag = 0
 while(1):
-
+    lag += 1
+    if lag > 5 and len(gesture_cache) > 0:
+        # flush the gesture
+        print gesture.lookup(gesture_cache)
+        gesture_cache = []
     # Take each frame
     _, frame = cap.read()
     # frame = cv2.imread('led.jpg')
@@ -26,7 +33,7 @@ while(1):
     hsv_white = cv2.cvtColor(white, cv2.COLOR_BGR2HSV)[0][0]
 
     lower = np.array([hsv_orange[0]-20, 100, 100])
-    upper = np.array([hsv_orange[0] + 80, 255, 255])
+    upper = np.array([hsv_orange[0] + 20, 255, 255])
     lw = np.array([0,0,0], dtype=np.uint8)
     uw = np.array([10,20,255], dtype=np.uint8)
     
@@ -39,18 +46,25 @@ while(1):
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
-    
-    
+
     centres = []
     contours, hierarchy = cv2.findContours(mask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for i in range(len(contours)):
-        if cv2.contourArea(contours[i]) < 500:
+        rect = cv2.minAreaRect(contours[i])
+        rect_perim = ((rect[0][0], rect[0][1]), (rect[1][0], rect[1][1]), rect[2])
+        box = cv2.cv.BoxPoints(rect_perim)
+        import pdb
+        pdb.set_trace()
+        # cv2.Rectangle(mask, (box[0])
+        if cv2.contourArea(contours[i]) < 100:
             continue
         moments = cv2.moments(contours[i])
         if moments['m00'] > 0:
             centres.append((int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00'])))
             cv2.circle(mask, centres[-1], 3, (0, 255, 0), -1)
             print centres[-1]
+            gesture_cache.append(centres[-1])
+            lag = 0
 
 
 
