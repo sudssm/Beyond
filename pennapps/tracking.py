fa27e0cfbbd55daf0a4c77518babc20b5b7a9b3d
feefles <eyeung@seas.upenn.edu>
2015-09-05 21:12:47 -0400

    post demo code

--- pennapps/tracking.py
+++ pennapps/tracking.py
@@ -9,7 +9,7 @@ gesture_cache = []
 lag = 0
 while(1):
     lag += 1
-    if lag > 5 and len(gesture_cache) > 0:
+    if lag > 10 and len(gesture_cache) > 0:
         # flush the gesture
         print gesture.lookup(gesture_cache)
         gesture_cache = []
@@ -34,8 +34,10 @@ while(1):
 
     lower = np.array([hsv_orange[0]-20, 100, 100])
     upper = np.array([hsv_orange[0] + 20, 255, 255])
+    
     lw = np.array([0,0,0], dtype=np.uint8)
-    uw = np.array([10,20,255], dtype=np.uint8)
+    uw = np.array([255,255,50], dtype=np.uint8)
+
     
     # Threshold the HSV image to get only specified colors
     mask = cv2.inRange(hsv, lower, upper)
@@ -49,17 +51,34 @@ while(1):
 
     centres = []
     contours, hierarchy = cv2.findContours(mask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    for i in range(len(contours)):
-        rect = cv2.minAreaRect(contours[i])
-        rect_perim = ((rect[0][0], rect[0][1]), (rect[1][0], rect[1][1]), rect[2])
-        box = cv2.cv.BoxPoints(rect_perim)
-        import pdb
-        pdb.set_trace()
-        # cv2.Rectangle(mask, (box[0])
-        if cv2.contourArea(contours[i]) < 100:
-            continue
-        moments = cv2.moments(contours[i])
-        if moments['m00'] > 0:
+    for idx, contour in enumerate(contours):
+        area = cv2.contourArea(contour) 
+        if area < 10: 
+            continue       
+
+        # rect = cv2.minAreaRect(contours[i])
+        # box = cv2.cv.BoxPoints(rect)
+        # box = np.int0(box)
+        
+        # crop = frame[box[0][0]:box[0][1], box[1][0]:box[1][1]]
+        # m = cv2.inRange(crop, lw, uw)
+        # if m != None:
+        #     print sum(sum(m))
+        # circles = cv2.HoughCircles(mask, cv.CV_HOUGH_GRADIENT, 1, 20, param1=40,\
+                # param2=20, minRadius=0, maxRadius=0)
+        if area > 100:
+            # circles = np.uint16(np.around(circles))
+            # if len(circles) > 1: 
+            #     continue
+            # for i in circles[0,:]:
+            #     # draw the outer circle
+            #     cv2.circle(mask,(i[0],i[1]),i[2],(0,255,0),2)
+            #     # draw the center of the circle
+            #     cv2.circle(mask,(i[0],i[1]),2,(0,0,255),3)
+            #     print i[0],i[1]
+            # import pdb
+            # pdb.set_trace()
+            moments = cv2.moments(contour)
             centres.append((int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00'])))
             cv2.circle(mask, centres[-1], 3, (0, 255, 0), -1)
             print centres[-1]
